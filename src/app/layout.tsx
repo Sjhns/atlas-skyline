fd4fc2ea431a2bf4b569af6e05d41481aac34e6f
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-10-08 15:22:13 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Nasa',
-  description: '',
-}
+  title: "Altas Skyline",
+  description: "Sistema de realidade virtual para viagens espaciais",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
